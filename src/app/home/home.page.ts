76ab237d4f0fd9e490927718c56b281d642eb0e6
phaalonso <phaalonso@gmail.com>
2020-12-16 10:55:28 -0300

    Verificação form

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public formulario: FormGroup;
+  // public isSubmited = false;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {
+    this.formulario = this.formBuilder.group({
+      name: new FormControl('', Validators.required)
+    });
+  }
+
+  submitForm() {
+    console.log('Submetido');
+    // this.isSubmited = true;
+    if (this.formulario.valid) {
+      // this.isSubmited = false;
+      console.log('Preencha todos os campos obrigatórios');
+      console.log(this.formulario.value);
+    }
+  }
 
 }
